4d4122f886d2b6463c14e46ac99a78b3ced73688
James Sulak <jsulak@gmail.com>
2012-12-02 15:58:51 -0600

    Epoch GMT and icon.

--- ConvertEpoch.popclipext/epoch.sh
+++ ConvertEpoch.popclipext/epoch.sh
@@ -1,7 +1,8 @@
 output=$(perl -e "print scalar(localtime($POPCLIP_TEXT))")
+output_gmt=$(perl -e "print scalar(gmtime($POPCLIP_TEXT))")
 osascript << eof
 tell application "System Events"
   activate 
-  display dialog "$output"
+  display dialog "$output Local \n$output_gmt GMT"
 end tell 
 eof
